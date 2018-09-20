daf22d7bffb1f5e89e70ec01579defbcb64c85cb
Kota <sandeshjkota@gmail.com>
2018-09-20 17:40:25 +0530

    Create tour.js

---
+++ tour.js
@@ -0,0 +1,32 @@
+// Define the tour!
+
+var tour = {
+      id: "hello-hopscotch",
+      steps: [
+        {
+          title: "Home",
+          content: "Home Page navigation",
+          target: ".left-floater",
+          placement: "right"
+        },
+        {
+          title: "Navigation Links",
+          content: "You can find the reference links categorized as per these tabs",
+          target: document.querySelector(".nav-tab-panel"),
+          placement: "right"
+        },
+    		{
+    			title: "Results",
+    			content: "You can find the respective reference links in this section",
+    			target: ".tab-container",
+    			placement: "bottom"
+    		}
+      ],
+      showPrevButton: true,
+      scrollTopMargin: 100
+    };
+
+// Start the tour!
+function StartTour(){
+    hopscotch.startTour(tour);    
+}
